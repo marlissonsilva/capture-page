1ed4dca713fc1beea94c7effee6380d39c029e38
Marlisson Silva <marlyssonsilva005@gmail.com>
2022-11-10 06:57:01 -0300

    Adicionei verificação de email, componente de loading

--- script.js
+++ script.js
@@ -1,34 +1,57 @@
-let btn = document.querySelector('#btn-submit')
+const btn = document.querySelector('#btn-submit');
+const card = document.querySelector('#card');
+const aviso = document.querySelector('#aviso')
 
 
 btn.addEventListener('click', submit);
 
+
 function submit(e) {
     e.preventDefault()
-    let card = document.querySelector('#card')
-    let aviso = document.querySelector('#aviso')
     let inputName = document.querySelector('input#name');
     let inputEmail = document.querySelector('input#email')
     let select = document.querySelector('#select')
     fullName = inputName.value
     email = inputEmail.value
+    const buttonLoad = document.querySelector('button')
+
+
 
     if (fullName === '' || email === '' || select.value == 'Estado') {
-        aviso.style.display = 'flex'
+        aviso.style.display = 'block'
+    }
+    else if (validateEmail(email) == false) {
+        alert('Email inválido')
+        email.style.backgroundColor = 'red'
     } else {
-        aviso.style.display = 'none'
-        card.innerHTML = `<h3>Seus dados foram cadastrados com sucesso!</h3>`
-        card.innerHTML += `<h4>Nome: ${fullName}</h4>`
-        card.innerHTML += `<h4>Email: ${email}</h4>`
-        card.innerHTML += `<h4> Cidade: ${select.value}</h4>`
-        card.style.padding = '15px'
-        card.style.height = '40%'
+        buttonLoad.style.display = 'block'
+        setInterval(() => {
+            buttonLoad.style.display = 'none'
+            aviso.style.display = 'none'
+            card.innerHTML = `<h3>Seus dados foram cadastrados com sucesso!</h3>
+                            <h4>Nome:<span> ${fullName} </span></h4>
+                            <h4>Email:<span> ${email}</span></h4>
+                            <h4> Cidade:<span> ${select.value}</span></h4>
+                            <a href="./images/banner.png" download="./images/banner.png" >Faça download do arquivo</a>`
+            card.style.padding = '2rem';
+            document.querySelector('#fade').style.display = 'block'
+        }, 600)
+
         setTimeout(() => {
             hide()
 
-        }, 3000);
+        }, 6000);
     }
 }
+function validateEmail(email) {
+    var reg = /\S+@\S+\.\S+/;  // Expressao regular
+    return reg.test(email);
+    // (método) .test: boolean
+    // Retorna um valor booleano que indica se existe ou não um padrão em uma string pesquisada.
+}
+
+
+
 
 function hide() {
     document.querySelector('#card').style.display = "none"
